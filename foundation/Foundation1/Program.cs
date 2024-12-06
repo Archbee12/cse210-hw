c7c89d326da63def976c71ce2876ac5952633f23
Archbee12 <debbieokon32@gmail.com>
2024-12-06 10:52:12 +0100

    Exe Picture

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -4,24 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-       Video video1 = new Video("Introduction to C#", "Alice", 300);
-       Video video2 = new Video("Python Basic to Advanced Tutorial", "Deborah", 320);
+        Video video1 = new Video("Introduction to C#", "Peniel Sweet", 300);
+        Video video2 = new Video("Python Basic to Advanced Tutorial", "Deborah S", 320);
+        Video video3 = new Video("UI/UX Design - Advanced Lesson", "Ilerioluwase General", 220);
 
-    //    Adding comments to videos
+        //    Adding comments to videos
         video1.AddComment(new Comment("John,", "I love how you breakdown the videos to make it simple to understand"));
-        video1.AddComment(new Comment("John,", "Interesting, I love the tutorials. twas very explanatory"));
+        video2.AddComment(new Comment("Damilola,", "Interesting, I love the tutorials. twas very explanatory"));
+        video3.AddComment(new Comment("Bernard,", "Well concised and explanatory video"));
 
-        video2.AddComment(new Comment("Vivian", "I learned a lot from this video."));
+        video1.AddComment(new Comment("Vivian", "I learned a lot from this video."));
         video2.AddComment(new Comment("Sarah", "It looks advanced for me to understand."));
+        video3.AddComment(new Comment("Gift", "Wow, now i understand ui/ux greatly."));
+
+        video1.AddComment(new Comment("DeborahM", "well detailed video, Keep it up"));
+        video2.AddComment(new Comment("Sarah", "This is video is the best for last-minute review. I loveeee it"));
+        video3.AddComment(new Comment("Gift", "Epic.. Is there a video on the basics, I am kind of a slow learner"));
 
         // Adding videos to list
-        List<Video> _videos = new List<Video> {video1, video2};
+        List<Video> _videos = new List<Video> { video1, video2, video3 };
 
         foreach (var video in _videos)
         {
             Console.WriteLine($"Title: {video._title}");
             Console.WriteLine($"Author: {video._author}");
-            Console.WriteLine($"Length: {video._length}");
+            Console.WriteLine($"Length: {video._length} seconds");
             Console.WriteLine($"Numbers of Comment: {video.GetCommentNumber()}");
 
             Console.WriteLine("Comments:");
